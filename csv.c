257716f39b76c95250723a7ae05382ab276a3927
Jonas S Karlsson <jsk@yesco.org>
2022-10-18 20:48:54 +0700

    getting there, fixing end backslash vs newline in quoted string

--- csv.c
+++ csv.c
@@ -2,6 +2,109 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <strings.h>
+#include <ctype.h>
+
+
+
+// freadCSV reads from FILE a STRING of MAXLEN
+// and sets a DOUBLE if it's a number.
+//
+// Returns one of these or 0 at EOF
+#define RNEWLINE 10
+#define RNULL 20
+#define RNUM 30
+#define RSTRING 40
+
+int freadCSV(FILE* f, char* s, int max, double* d) {
+  int c, q= 0, typ= 0;
+  char* r= s;
+  *r= 0; max--; // zero terminate 1 byte
+  while((c= fgetc(f))!=EOF &&
+	(c!=',' || q>0) && (c!='\n' || q>0)) {
+    if (c=='\r') continue; // DOS
+    if (c==0) return RNEWLINE;
+    if (c==q) { q= -q; continue; } // "foo" bar, ok!
+    if (c==-q) q= c;
+    else if (!typ && !q && (c=='"' || c=='\''))
+      { q= c; typ= RSTRING; continue; }
+    if (!typ && isspace(c)) continue;
+    if (c=='\\') if ('\n'==(c= fgetc(f))) c= fgetc(f);
+    if (max>0) {
+      *r++= c;
+      *r= 0;
+      max--;
+      typ= RSTRING; // implicit if not quoted
+    } else ; // TODO: indicate truncation?
+  }
+  // have value
+  if (c=='\n') ungetc(0, f); // next: RNEWLINE
+  if (c==EOF) if (!typ) return 0;
+  // number?
+  char* end;
+  *d= strtod(s, &end);
+  if (end!=s) {
+    // remove trailing spaces and reparse
+    // but only if no other chars
+    int l= strspn(end, " ");
+    if (end+l==s+strlen(s)) *end=0;
+    *d= strtod(s, &end);
+    if (s+strlen(s)==end) return RNUM;
+  }
+  // Null if empty string
+  return typ?typ:RNULL;
+}
+
+// NOTES:
+//   "foo""bar"","foo\"bar",'foo"bar' quoting
+//   unquoted leading spaces removed
+//   string that is (only) number becomes number
+//   foo,,bar,"",'' gives 3 RNULLS
+//
+// WARNING: moedifies f
+int sreadCSV(char** f, char* s, int max, double* d) {
+  int c, q= 0, typ= 0;
+  char* r= s;
+  *r= 0; max--; // zero terminate 1 byte
+  while((c= *(*f)++) &&
+	(c!=',' || q>0) && (c!='\n' || q>0)) {
+    if (c=='\r') continue; // DOS
+    if (c==0) return 0;
+    if (c==q) { q= -q; continue; } // "foo" bar, ok!
+    if (c==-q) q= c;
+    else if (!typ && !q && (c=='"' || c=='\''))
+      { q= c; typ= RSTRING; continue; }
+    if (!typ && isspace(c)) continue;
+    if (c=='\\') if ('\n'==(c= *(*f)++)) {
+	printf("QUOTE");
+	c= *(*f)++;
+      }
+    if (max>0) {
+      *r++= c;
+      *r= 0;
+      max--;
+      typ= RSTRING; // implicit if not quoted
+    } else ; // TODO: indicate truncation?
+  }
+  // have value
+  if (!c) if (!typ) return 0;
+  // number?
+  char* end;
+  *d= strtod(s, &end);
+  if (end!=s) {
+    // remove trailing spaces and reparse
+    // but only if no other chars
+    int l= strspn(end, " ");
+    if (end+l==s+strlen(s)) *end=0;
+    *d= strtod(s, &end);
+    if (s+strlen(s)==end) return RNUM;
+  }
+  // Null if empty string
+  return typ?typ:RNULL;
+}
+
+
+
 
 int min(int a, int b) { return a<b?a:b; }
 int max(int a, int b) { return a>b?a:b; }
@@ -36,7 +139,7 @@ char* csvgetline(FILE* f) {
 	switch(*++s) {
 	case '\r': s++; // \n follows
 	case '\n':
-	  if (*++s) continue;
+	  if (*++s) {++s; continue;}
 	  else inquote= 1; // read more
 	case 0: inquote= 1;
 	default: continue;
@@ -90,8 +193,33 @@ int main(int argc, char** argv) {
 
   {
     FILE* f= fopen("nl.csv", "r");
+    char v[1024];
+    double d;
+    int r;
+    while((r= freadCSV(f, v, sizeof(v), &d))) {
+      if (r==RNULL) printf("NULL   ");
+      if (r==RNUM) printf("%lg   ", d);
+      if (r==RSTRING) printf(">%s<   ", v);
+      if (r==RNEWLINE) printf("\n");
+    }
+  }
+
+  {
+    FILE* f= fopen("nl.csv", "r");
     char* s= csvgetline(f);
-    printf(">>>%s<<<\n", s);
+    printf("\n>>>%s<<<\n\n", s);
+
+    char v[1024];
+    double d;
+    int r;
+    char* ss= s;
+    while((r= sreadCSV(&ss, v, sizeof(v), &d))) {
+      if (r==RNULL) printf("NULL   ");
+      if (r==RNUM) printf("%lg   ", d);
+      if (r==RSTRING) printf(">%s<   ", v);
+      if (r==RNEWLINE) printf("\n");
+    }
+
     free(s);
   }
 }
