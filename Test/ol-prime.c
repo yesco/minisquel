3022ea9540801d09360f3595875cdeba13d9e596
Jonas S Karlsson <jsk@yesco.org>
2022-11-13 15:45:26 +0700

    objectlog, measure ovehead of indirection/dispatch

--- Test/ol-prime.c
+++ Test/ol-prime.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  long p= argc>1 ? atol(argv[1]) : 2124679;
+  long pp= argc>1 ? atol(argv[1]) : 2124679;
 
-  for(long a=2; a<=100; a++) {
-    long b= p-1;
-    for(long c=2; c<=b; c++) {
-      long r= p % c;
-      if (r==0) {
-	printf("%ld is not prime says %ld\n",
-	       p, c);
+  if (0) {
+    // closer by using indirection
+    // 745 s (100 ms slower, 13 % !)
+    long aa, bb, cc, rr;
+    long *a=&aa, *b=&bb, *c=&cc, *r=&rr, *p=&pp;
+    for(*a=2; *a<=100; (*a)++) {
+      *b= *p-1;
+      for(*c=2; *c<=*b; (*c)++) {
+	*r= *p % *c;
+	if (*r==0) {
+	  printf("%ld is not prime says %ld\n",
+		 *p, *c);
+	}
+      }
+    }
+  } else {
+    // 658 ms
+    for(long a=2; a<=100; a++) {
+      long b= pp-1;
+      for(long c=2; c<=b; c++) {
+	long r= pp % c;
+	if (r==0) {
+	  printf("%ld is not prime says %ld\n",
+		 pp, c);
+	}
       }
     }
   }
