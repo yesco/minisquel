f6d5250040175fcacaca962026da87ff923ab807
Jonas S Karlsson <jsk@yesco.org>
2024-08-06 04:45:20 +0700

    panda: extract all konst first

--- panda.c
+++ panda.c
@@ -3,6 +3,34 @@
 #include <stdlib.h>
 #include <assert.h>
 
+// PANDA LANGUAGE
+// ==============
+// pandalang.org is an online enviornment implementing
+// the Panda Language.
+
+// This is a C language implementation, of a simple parser,
+// with a no-frills grammar. It even simplifies panda.
+
+// An expression is a number or a string:
+//
+//     42
+//     'foobar'
+//
+// A function calls is
+//
+//     42 sqrt
+//
+// A composite point-free style of function alls
+//
+//     1 to 10 sqr lt 55
+//
+// For all numbers from 1 to 10.
+// Square them.
+// Keep the ones less than 55.
+// Then print it.
+//
+// 
+
 int nval= 0;
 
 void spc(char** p) {
@@ -10,34 +38,55 @@ void spc(char** p) {
 }
 
 // TODO: reuse same constants...
-int num(char** s) {
-  spc(s);
-  char* p= *s;
-  if (!*p || !isdigit(*p) && *p!='-' && *p!='.') return 0;
-
-  // we don't care if it's well-formed
-  do {
-    p++;
-  } while(*p && (isdigit(*p) || *p=='.' || toupper(*p)=='e'));
-
-  nval++;
-  printf(": %.*s\n", (int)(p-*s), *s);
-  *s= p;
-  return nval;
-}
-
-// TODO: reuse same constants...
 int str(char** s) {
   return 0;
 }
 
+// TODO: scan and store all const num/'strings'/:foo
+#define KMAX 1024
+char* konst[KMAX];
+int nk= 0, klen[KMAX];
+
+void konstractor(char* p) {
+  while(*p) {
+    switch(*p) {
+    case ' ': case '\t': case '\n': case '\r': p++; break;
+    case '0'...'9': case '.': case '-': {
+      konst[nk]= p;
+      do {
+	p++;
+      } while (*p && (isdigit(*p) || *p=='.' || *p=='e'));
+      klen[nk]= p-konst[nk];
+      nk++;
+      break; }
+    case '"': case '\'': {
+      konst[nk]= p;
+      char e= *p;
+      do {
+	if (*p=='\\') p++;
+	p++;
+      } while(*p && *p!=e);
+      if (*p) p++;
+      klen[nk]= p-konst[nk];
+      nk++;
+      break; }
+      //case ':': p++; r= str(&p, ' '); break;
+    default: p++;
+    }
+  }
+}
+
+int ik= 0;
+
 int expr(char** p) {
-  int r= num(p);
-  if (!r) r= str(p);
-  // TODO: foo:bar as http://ibm.com foo:3
+  spc(p);
+  if (*p==konst[ik]) {
+    *p+= klen[ik];
+    return ik++;
+  }
   // TODO: paren expression
   // TODO: +-*/ math should use proper preceedence
-  return r;
+  return 0;
 }
 
 // a function has a preceeding argument O (or 0) lol
@@ -55,7 +104,9 @@ int fun(char** s, int o) {
   #define NPAR 16
   int par[NPAR]= {0};
   int npar= 0;
+
   // keep reading args
+  // TODO: allow fun(1 2 3) ?
   do {
     assert(npar<16);
     if (o) par[npar++]= o;
@@ -84,7 +135,16 @@ int panda(char** p) {
 }
 
 int main(int argc, char** argv) {
-  argv++; // skip program name
+  argv++; argc--; // skip program name
+
+  // Extract all constants
+  char** a= argv;
+  while(*a) konstractor(*a++);
+  for(int i=0; i<nk; i++)
+    printf(": %.*s\n", klen[i], konst[i]);
+
+  nval= nk;
+  
   while(*argv) {
     char* p = *argv++;
     // TODO: keep doing more expressions?
