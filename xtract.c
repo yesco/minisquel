a85d0dcd9e4173049ab9f3211f049af6aa657672
Jonas S Karlsson <jsk@yesco.org>
2024-08-12 04:00:35 +0700

    xtrace playing

--- xtract.c
+++ xtract.c
@@ -30,7 +30,10 @@ char* xscan(char* s, char t) {
 
     case ' ': case '\t': case '\r': case '\n': break;
       
-    case ')': case '}': case ']': return s;
+    case ',':
+    case ')': case '}': case ']':
+      printf("RETURNING: %s\n", s);
+      return s;
       
     case '\"': q++;
     case '\'': q++;
@@ -47,8 +50,13 @@ char* xscan(char* s, char t) {
     case '[': q++;
     case '{': q++;
     case '(': q++;
-      s= xscan(s+1, *s);
-      assert(*s==" ({["[q]); break;
+      printf("LIST: '%c'\n", *s);
+      do {
+	s= xscan(s+1, *s);
+	printf("NEXT ELEMENT: %s\n", s);
+      } while(*s==','); // TODO: space etc?
+      printf("sss> '%s' %d '%c'\n", s, q, " )}]"[q]);
+      assert(*s==" )}]"[q]); break;
 
     case ':': case '=':
       printf("--- '%.*s'\n", (int)(s-last), last);
@@ -57,8 +65,8 @@ char* xscan(char* s, char t) {
       break;
 
     // array
-    case ',': case ';': n++;
-      printf("--- [%d]\n", n); break;
+      //    case ',': case ';': n++;
+      //      printf("--- [%d]\n", n); break;
       
     default: printf("default>%.*s< '%c'\n", (int)(last-s), s, *s);
       
@@ -69,9 +77,110 @@ char* xscan(char* s, char t) {
   return 0;
 }
 
+char* xscan2(char* s);
+
+char* spc(char* s) {
+  while(s && isspace(*s)) s++;
+  return s;
+}
+
+char* xarr(char* s) {
+  do {
+    s= spc(s);
+    char* last= s;
+    s= xscan2(s);
+    s= spc(s);
+    if (*s==',') s++;
+  } while(*s!=']' && *s!=')');
+  assert(*s==']' || *s==')');
+  return s+1;
+}
+
+char* xsym(char* s);
+
+char* xdic(char* s) {
+  do {
+    s= spc(s);
+    char* key= s;
+    s= xsym(s);
+    if (key==s) break;
+    s= spc(s);
+    char* val= s= xscan2(s);
+    s= spc(s);
+    if (*s==',') s++;
+  } while(*s!='}');
+  assert(*s=='}');
+  return s+1;
+}
+
+char* xstr(char* s) {
+  char q= *s++;
+  while(*s && *s!=q) {
+    if (*s=='\\') s++;
+    s++;
+  }
+  assert(*s==q);
+  s++;
+  return s+1;
+}
+
+char* xnum(char* s) {
+  while(*s && (isdigit(*s) || strchr("+-eE.", *s))) s++;
+  return s;
+}
+
+char* xsym(char* s) {
+  // TODO: unicode use stopchars
+  while(*s && isalnum(*s)) 
+    s++;
+  assert(*s==':');
+  return s+1;
+}
+
+char* xscan2(char* s) {
+  s= spc(s);
+  if (!s || !*s) return 0;
+  switch(*s) {
+  case '(':
+  case '[': s= xarr(s+1); break;
+  case '{': s= xdic(s+1); break;
+  case '"': case '\'': s= xstr(s); break;
+  case '0'...'9': case '-': case '.': s= xnum(s); break;
+  default: s= xsym(s); break; }
+  return s;
+}
+
+char* xscan3(char* s) {
+  const static char bracks[] = "(){}[]";
+  //printf("--> %s\n", s);
+  s= spc(s);
+  if (!s || !*s) return s;
+  switch(*s) {
+  case ',': return xscan3(s+1); // TODO: remove?
+  case '(': case '{': case '[': {
+    char q= bracks[strchr(bracks, *s)-bracks+1];
+    s++;
+    do {
+      s= spc(s);
+      if (*s==',') s= spc(s+1);
+      char* a= s;
+      s= xscan3(s);
+      printf("> %.*s\n", (int)(s-a), a);
+      s= spc(s);
+    } while(*s && *s!=q);
+    assert(*s==q);
+    return s+1; }
+  case '"': case '\'': return xstr(s);
+  case '0'...'9': case '-': case '.': return xnum(s);
+  default: return xsym(s);
+  }
+}
+
 void scan(char* s) {
   printf("\n??? %s\n", s);
-  xscan(s, 0);
+  //xscan(s, 0);
+  //xscan2(s);
+  xscan3(s);
 }
 
 int main(int argc, char** argv) {
